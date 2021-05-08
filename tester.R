cb3b79048df026f8aaf500be3a9f6afeafefa084
ToniPleite <antonio_pleite@hotmail.com>
2021-05-08 23:09:14 +0200

    First set-up

---
+++ tester.R
@@ -0,0 +1,13 @@
+f_shuffle <- function(){
+
+}
+f_test <- function (){
+
+}
+f_readParameters <- function() {
+
+}
+# Main program
+print("Tell me the name of the file you'd like to be tested on:")
+test_file <- readline()
+df_test <- read.data.frame()
